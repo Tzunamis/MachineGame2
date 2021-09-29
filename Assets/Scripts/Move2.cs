da85cd7ab436242ef1bed13d86fd1799e96a0c39
RootsRockReggae <90478763+RootsRockReggae@users.noreply.github.com>
2021-09-29 00:05:16 -0700

    2nd player spawns on click
     but doesnt move through input

--- Assets/Scripts/Move2.cs
+++ Assets/Scripts/Move2.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Move2 : MonoBehaviour
 {
+    public float Speed = 2.0f;
+    private Vector2 moveInput = Vector2.zero;
+    [SerializeField] private float walking;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+
+    public void OnMove(InputAction.CallbackContext move)
+    {
+        moveInput = move.ReadValue<Vector2>();
+    }
+
+
+    private void Move()
+    {
+
+
+
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(moveInput * Speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(moveInput * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(moveInput * Speed * Time.deltaTime);
+
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.Translate(moveInput * Speed * Time.deltaTime);
+
+        }
+
+
+
+    }
+
 }
